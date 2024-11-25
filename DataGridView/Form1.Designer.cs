afeb831dc7cc092472b6495b30b9dee6740b4655
Carat3231 <yuipovvv7@gmail.com>
2024-11-26 03:21:31 +0900

    fisrt commit

---
+++ DataGridView/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fac_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fac_name_abbr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fac_name_full = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.fac_id,
+            this.fac_name_abbr,
+            this.fac_name_full});
+            this.dataGridView1.Location = new System.Drawing.Point(147, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(427, 221);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fac_id
+            // 
+            this.fac_id.HeaderText = "fac_id";
+            this.fac_id.MinimumWidth = 6;
+            this.fac_id.Name = "fac_id";
+            this.fac_id.Width = 125;
+            // 
+            // fac_name_abbr
+            // 
+            this.fac_name_abbr.HeaderText = "fac_name_abbr";
+            this.fac_name_abbr.MinimumWidth = 6;
+            this.fac_name_abbr.Name = "fac_name_abbr";
+            this.fac_name_abbr.Width = 125;
+            // 
+            // fac_name_full
+            // 
+            this.fac_name_full.HeaderText = "fac_name_full";
+            this.fac_name_full.MinimumWidth = 6;
+            this.fac_name_full.Name = "fac_name_full";
+            this.fac_name_full.Width = 125;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(293, 289);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(293, 332);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fac_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fac_name_abbr;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fac_name_full;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DataGridView/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        private void LoadData() {
+
+            string connectString = "Data Source  = CARAT; Initial Catalog = DataGridView; Integrated Security = true;"; 
+            SqlConnection myConnection = new SqlConnection(connectString);
+            myConnection.Open();
+
+            string query = "SELECT * FROM Group_IS211 ORDER BY id";
+
+            SqlCommand command = new SqlCommand(query, myConnection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<string[]> data = new List<string[]> ();
+
+            while (reader.Read()) {
+                data.Add(new string[3]);
+                data[data.Count - 1][0] = reader["id"].ToString();
+                data[data.Count - 1][1] = reader["name_students"].ToString();
+                data[data.Count - 1][2] = reader["secondName_students"].ToString();
+
+            }
+            reader.Close();
+            myConnection.Close();
+
+            foreach (string[] s in data)
+                dataGridView1.Rows.Add(s);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstName = textBox1.Text;
+            string lastName = textBox2.Text;
+
+            string connectionString = "Data Source  = CARAT; Initial Catalog = DataGridView; Integrated Security = true;";
+            using (SqlConnection myConnection = new SqlConnection(connectionString)) { 
+                myConnection.Open();
+                string query = "INSERT INTO Group_IS211 (name_students, secondName_students) VALUES (@Fname, @Sname); SELECT SCOPE_IDENTITY();";
+                using (SqlCommand command = new SqlCommand(query, myConnection)) {
+                    command.Parameters.AddWithValue("@Fname", firstName);
+                    command.Parameters.AddWithValue("@Sname", lastName);
+
+                    int newId = Convert.ToInt32(command.ExecuteScalar());
+
+                    dataGridView1.Rows.Add(new object[] { newId, firstName, lastName });
+                }
+                textBox1.Clear();
+                textBox2.Clear();
+            
+            }
+        }
+    }
+}
